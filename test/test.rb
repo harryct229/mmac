353b8e37d3a4746a210ab6cf57139633f99892f1
Thanh Nguyen <congthanh991@gmail.com>
2013-11-13 22:50:48 +0700

    optimized

--- test/test.rb
+++ test/test.rb
@@ -1,7 +1,7 @@
 require_relative '../lib/mmac'
 
 puts 'Run program...'
-framework = Mmac::Framework.new(File.dirname( __FILE__ ) + '/file/data.txt', 0.6, 0.8)
+framework = Mmac::Framework.new(File.dirname( __FILE__ ) + '/file/data.txt', 0.2, 0.4)
 framework.run
 
 puts 'Run Data Test'
